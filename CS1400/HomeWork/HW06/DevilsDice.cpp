c086d22adb382677920b56f25ba2a0be3d099b12
Brandon Larsen <Brandon Larsen>
2016-10-10 12:39:20 -0600

    Additional changes to devils dice still work in progress.

--- CS1400/HomeWork/HW06/DevilsDice.cpp
+++ CS1400/HomeWork/HW06/DevilsDice.cpp
@@ -6,21 +6,13 @@
 
 using namespace std;
 
-const int MINSCALEVALUE = 0;
-const int MAXSCALEVALUE = 100;
-
-int diceRoll();
-void welcome();
-void logic();
-void printScreen();
-
-
 int main(){
   int pCurr = 0;
   int dCurr = 0;
   int player = 1;
   int numAdd = 0;
-  int turnScore =0;
+  int turnScore = 0;
+  int lineNum = 0;
   char r = 'x';
 #ifdef JARVIS
   srand(0);
@@ -28,123 +20,69 @@ int main(){
   srand(time(NULL));
 #endif
 
-welcome();
-while(r!='f'){
-    printScreen();
-    cout<<"[s]et desired temp, [w]ait, or [q]uit: ";
+if (player == 1){
+  printScreen(pCurr, dCurr, turnScore);
+  cout << "Hold[h], roll[r], or forfeit[f]: ";
+  cin >> r;
+  while( numAdd != 1 ){
+    if ( r == 'r' ){
+      numAdd = die();
+        if (numAdd != 1){
+        turnScore += numAdd;
+        printScreen(pCurr,dCurr,turnScore);
+        cout << "Hold[h], roll[r], or forfeit[f]: ";
+        cin >> r;
+        cout << endl;
+        cout << "You rolled a "<< numAdd << endl;
+      } else if (numAdd == 1){
+        player = 2;
+        numAdd = 0;
+        printScreen(pCurr,dCurr,turnScore);
+        cout << "Hold[h], roll[r], or forfeit[f]: ";
+        cout << endl;
+        cout << "Your rolled a 1 :(" << endl;
+      }
+    } else if (r == 'h'){
+      pCurr += turnScore;
+      printScreen(pCurr,dCurr,turnScore);
+      cout << "Hold[h], roll[r], or forfeit[f]: ";
+      cout << endl;
+      cout << "You banked " << turnScore << "points."<< endl;
+      player = 2;
+    }
   }
-//printLine();
-//logic();
+  if( numAdd == 1){
+    player = 2;
+  }
+} else if (player == 2){
+  while ( numAdd != 1){
+    if (pCurr == dCurr){
+      turnScore += numAdd;
+      numAdd = die();
+      if( turnScore >= 21){
+        dCurr += turnScore;
+        player = 1;
+        printScreen(pCurr,dCurr,turnScore);
+        cout << "Hold[h], roll[r], or forfeit[f]: ";
+        cout << "Devil got " << numAdd << " points\n";
+      }
+    } else if ( pCurr >= dCurr ){
+      turnScore += numAdd;
+      numAdd = die();
+      if (turnScore >= 30){
+        turnScore += dCurr;
+        player = 1;
+        printScreen(pCurr,dCurr,turnScore);
+        cout << "Hold[h], roll[r], or forfeit[f]: ";
+        cout << "Devil got " << numAdd << " points\n";
+    }
+  }
+  if (numAdd == 1 ){
+    player = 1;
+  }
+}
   // Call your functions to play your game here...
   return 0;
 }
 
-int diceRoll(){
-  int die;
-  die = (rand()%6)+1;
-  return die;
-}
-
-//calculate dice roll and add to current score
-void logic(int pCurr, int dCurr, int player, int numAdd,int turnScore, int die){
-  char r;
-  cin >> r;
-  if (r == 'r'){
-    diceRoll();
-  }
-  // if your turn
-  while (player == 1){
-    numAdd = 0;
-    while (die != 1 && r == 'r'){
-      // player rolls
-      cout << "You rolled a "<< die << "!";
-      numAdd += die;
-      turnScore = numAdd + pCurr;
-      cin >> r;
-      }
-    if (die != 1 && r == 'h'){
-      // if hold bank the points and switch to devils turn
-      cout << "You banked " << numAdd << " points\n";
-      numAdd += pCurr;
-      player = 2;
-    } else if ( die == 1 ){
-      // if 1 rolled switch to other devil
-      cout << "You rolled a 1 :(\n";
-      player = 2;
-    }
-  }
-  // player 2 equals the devil
-  while (player == 2){
-    if (pCurr == dCurr){
-      // roll until the score devil reaches at least 21 or 100
-      numAdd = 0;
-      while(die != 1 && dCurr != 100){
-        numAdd += die;
-        if (numAdd >= 21){
-          cout << "Devil got " << numAdd << " points\n";
-          dCurr += numAdd;
-        } else {
-          numAdd += die;
-          dCurr += die;
-        }
-      }
-    }
-    if (pCurr >= dCurr){
-      numAdd = 0;
-      while(die != 1){
-        numAdd += die;
-        if (numAdd >= 30 && dCurr != 100){
-          numAdd += dCurr;
-          cout << "Devil got " << numAdd << " points\n";
-        }
-      }
-    }
-  }
-  if (die == 1){
-    numAdd = 0;
-    if (player == 1){
-      player = 2;
-    } else if (player == 2 ){
-      player = 1;
-    }
-  }
-}
-
-void welcome(){
-  cout << "---- Welcome to Devil's Dice! ----" << endl;
-  for(int i=MINSCALEVALUE;i<=MAXSCALEVALUE;i+=10){
-    //printLine();
-}
-}
-
-void printScreen(int lineNum, int pCurr, int dCurr, int turnScore, int player){
-    while (player == 1){
-      if( lineNum >= pCurr && lineNum <= turnScore ){
-        cout << setw(5) << pCurr << ">";
-      } else {
-        cout << setw(5) << " ";
-      }
-      cout << lineNum;
-    }
-    if (lineNum >= pCurr && lineNum < turnScore){
-      cout << left << setw(5) << "< ";
-      cout << turnScore;
-    }
-      cout << endl;
-
-    while (player == 2){
-      if( lineNum >= dCurr && lineNum <= turnScore ){
-        cout << setw(8) << pCurr << ">";
-      } else {
-        cout << setw(15) << " ";
-      }
-
-      cout << lineNum;
-    if (lineNum >= dCurr && lineNum < turnScore){
-      cout << right << setw(15) << "< ";
-      cout << turnScore;
-    }
-      cout << endl;
-    }
-  }
   //test
--- CS1400/HomeWork/HW06/devilDice.cpp
+++ CS1400/HomeWork/HW06/devilDice.cpp
@@ -6,6 +6,61 @@
 
 using namespace std;
 
+const int MINSCALEVALUE = 0;
+const int MAXSCALEVALUE = 100;
+
+void printPlayerLine(int lineNumber,int pCurr, int dCurr, int turnScore){
+      if(lineNumber>=pCurr && lineNumber < pCurr+10){
+          cout << left << " "<< setw(3);
+          cout << setw(3) << pCurr << ">";
+      } else {
+          cout << setw(5) << " ";
+      }
+
+      // print
+      cout << "--" << lineNumber ;
+
+      // print turn value
+      if(lineNumber>=turnScore && lineNumber < turnScore+10){
+          cout << right <<setw(2) << "<";
+          cout << setw(3) << turnScore;
+      } else {
+          // not the right line number; print spaces instead
+          cout<< setw(5) << " ";
+      }
+
+      cout << setw(15) << lineNumber << right<<"--";
+
+      if(lineNumber>=dCurr && lineNumber < dCurr+10){
+          cout << right <<setw(3) << "<";
+          cout << setw(2)<<dCurr;
+      } else {
+          // not the right line number; print spaces instead
+          cout<< setw(5) << " ";
+      }
+
+      cout << endl;
+}
+
+void printScreen(int pCurr, int dCurr, int turnScore){
+    cout << "---- Welcome to Devil's Dice! ----";
+    cout << "\n\n";
+    cout << setw(5) <<" "<< "Player" << setw(15) << " " << "Devil\n";
+    cout << setw(5) <<" "<< "------" << setw(15) << " " << "-----\n";
+    for(int i=MAXSCALEVALUE;i>=MINSCALEVALUE;i-=10){
+        printPlayerLine(i, pCurr, dCurr, turnScore);
+    }
+}
+
+void playerTurn(){
+
+}
+
+int die(){
+  return (rand()%6)+1;
+}
+
+
 int main()
 {
 #ifdef JARVIS
@@ -14,6 +69,73 @@ int main()
   srand(time(NULL));
 #endif
 
+char r = 'z';
+int pCurr = 0;
+int dCurr = 0;
+int turnScore;
+int player =1;
+int numAdd;
+
+while(r != 'f'){
+    printScreen(pCurr, dCurr, turnScore);
+    cout << "Hold[h], roll[r], or forfeit[f]: ";
+    cin >> r;
+    while(player == 1){
+      if (r == 'r'){
+        numAdd = die();
+          if (numAdd != 1){
+          turnScore += numAdd;
+          printScreen(pCurr,dCurr,turnScore);
+          cout << "Hold[h], roll[r], or forfeit[f]: ";
+          cin >> r;
+          cout << endl;
+          cout << "You rolled a "<< numAdd << endl;
+        } else if (numAdd == 1){
+          turnScore = 0;
+          printScreen(pCurr,dCurr,turnScore);
+          cout << "Hold[h], roll[r], or forfeit[f]: ";
+          cout << endl;
+          cout << "Your rolled a 1 :(" << endl;
+          player = 2;
+        }
+      } else if (r == 'h'){
+        pCurr += turnScore;
+        printScreen(pCurr,dCurr,turnScore);
+        cout << "Hold[h], roll[r], or forfeit[f]: ";
+        cout << endl;
+        cout << "You banked " << turnScore << "points."<< endl;
+        player = 2;
+      } else if (player == 2){
+      numAdd += die();
+      while ( numAdd != 1){
+        if (pCurr == dCurr){
+          turnScore += numAdd;
+          numAdd = die();
+          if( turnScore >= 21){
+            dCurr += turnScore;
+            printScreen(pCurr,dCurr,turnScore);
+            cout << "Hold[h], roll[r], or forfeit[f]: ";
+            cout << "Devil got " << numAdd << " points\n";
+            player = 1;
+          }
+        } else if ( pCurr >= dCurr ){
+          turnScore += numAdd;
+          numAdd = die();
+          if (turnScore >= 30){
+            turnScore += dCurr;
+            printScreen(pCurr,dCurr,turnScore);
+            cout << "Hold[h], roll[r], or forfeit[f]: ";
+            cout << "Devil got " << numAdd << " points\n";
+            player = 1;
+          }
+        }
+      }
+      if (numAdd == 1 ){
+        player = 1;
+      }
+    }
+  }
+}
   // Call your functions to play your game here...
 
   return 0;
