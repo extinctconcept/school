c17e50f2b8a49cbc2e3aec12d882e724bd12377c
Brandon Larsen <Brandon Larsen>
2016-10-19 13:17:48 -0600

    Adding Mandelbrot homework and input files

--- CS1400/HomeWork/HW08/mandelbrot.cpp
+++ CS1400/HomeWork/HW08/mandelbrot.cpp
@@ -1,7 +1,7 @@
 /************************
-* A#:
+* A#: A01224769
 * Course: CS1400
-* Section:
+* Section: 7
 * HW#: 8
 ***********************/
 
@@ -41,9 +41,23 @@ Color getPixelColor(Color color1, Color color2, int iterations, int maxIteration
 int main()
 {
   // Read in config file location from user
+  string filename;
+  ifstream input;
+  ofstream output;
+  string line;
+  cout << "Mandelbrot Config File: ";
+  cin >> filename;
+  input.open(filename.c_str());
 
   // Read config file contents into MandelbrotConfig struct instance
-
+  if (input.is_open())
+    {
+      while ( getline (input,line) )
+      {
+        cout << line << '\n';
+      }
+      input.close();
+    }
   // Compute and write specified mandelbrot image to PPM file
 
   return 0;
